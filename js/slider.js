bd1da26109582a048ec81ac7611e8cbe454119ef
DDA2-0 <54625439+DDA2-0@users.noreply.github.com>
2020-10-31 03:10:14 +0900

    1031

---
+++ js/slider.js
@@ -0,0 +1,8 @@
+var appendNumber = 4;
+var prependNumber = 1;
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 4.3,
+  centeredSlides: true,
+  paginationClickable: true,
+  spaceBetween: 30,
+});

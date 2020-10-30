dd90f41e70fdcc4b9b68a520cde756eaed2f6848
DDA2-0 <54625439+DDA2-0@users.noreply.github.com>
2020-10-30 16:56:03 +0900

    1030

---
+++ js/image_random.js
@@ -0,0 +1,10 @@
+(function($){
+      $(document).ready(function () {
+            $('.about_box').click(function(){
+                  location.assign("../html/about.html");
+            });
+            $('.service_box').click(function(){
+                  location.assign("../html/service.html");
+            });
+      });
+})(jQuery);

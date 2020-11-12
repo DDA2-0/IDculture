5d3ec76da9ba3288660fcbdf31ed358469b49ab8
DDA2-0 <54625439+DDA2-0@users.noreply.github.com>
2020-11-13 02:33:49 +0900

    1113

--- js/mouse.js
+++
@@ -1,7 +0,0 @@
-(function($){
-
-      $(window).mousedown(function (e) {
-            $('.click_pop').remove();
-            $('body').append('<span class="click_pop" style="left:' + e.pageX + 'px;top:' + e.pageY +'px;"><span/><span/><span/><span/></span>');
-        });
-})(jQuery);
